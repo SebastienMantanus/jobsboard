521d04060ce40f4dafab33213151c70c1cb4da2a
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2022-12-15 21:52:09 +0100

    commit first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Jobs from "./components/Jobs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Header title="The Job Board" />
       </header>
+      <body>
+        <jobs>
+          <Jobs
+            className="blue"
+            title="Full Time Sales Associate - Sydney Boutique"
+            contractType="CDI"
+            country="France"
+            city="Paris"
+          />
+        </jobs>
+      </body>
+      <footer>
+        <div>
+          <p>Made with React at Le Reacteur By Nono</p>
+        </div>
+      </footer>
     </div>
   );
 }
